abfbf2350707b9f2f62316e3af31c702babba05e
Tim Steinbach <tim@nequissimus.com>
2019-09-08 12:23:26 -0400

    Add test for local import list

---
+++ scalafix/input/src/main/scala/fix/localimportlist.scala
@@ -0,0 +1,16 @@
+/*
+ rule = SortImports
+ SortImports.blocks = [
+ "java",
+ "scala"
+ ]
+ */
+import java.math.BigInteger
+import java.util.Map
+
+import scala.collection._, scala.util._
+
+object LocalImportList {
+  import List._, Set._
+  // Add code that needs fixing here.
+}
---
+++ scalafix/output/src/main/scala/fix/localimportlist.scala
@@ -0,0 +1,9 @@
+import java.math.BigInteger
+import java.util.Map
+
+import scala.collection._, scala.util._
+
+object LocalImportList {
+  import List._, Set._
+  // Add code that needs fixing here.
+}
