ee7bdd7ebbeb1bc9ecba74add33ffabaf5592cbd
Tuure Savuoja <tuure.savuoja@trademarknow.com>
2019-10-02 17:13:52 +0300

    reorder imports in the importlist test so that scalafix actually has to re-order the imports which then fails with the bug #9

--- scalafix/input/src/main/scala/fix/importlist.scala
+++ scalafix/input/src/main/scala/fix/importlist.scala
@@ -5,10 +5,10 @@
  "scala"
  ]
  */
+import scala.collection._, scala.util._
+
 import java.math.BigInteger
 import java.util.Map
-
-import scala.collection._, scala.util._
 
 object ImportList {
   // Add code that needs fixing here.
