92180827f798d4c176b6f9b8b3396c42f3f7b58f
Tuure Savuoja <tuure+oss@tuuresavuoja.net>
2019-10-03 15:44:58 +0300

    sort config blocks by length to make sure longest block prefixes take precedence, closes #8 (#12)

---
+++ scalafix/output/src/main/scala/fix/subprefixes.scala
@@ -0,0 +1,12 @@
+package fix
+
+import scala.io.Source
+
+import scala.collection.IndexedSeq
+import scala.collection.Seq
+
+import scala.annotation._
+
+import java.io.File
+
+object SubPrefixes {}
