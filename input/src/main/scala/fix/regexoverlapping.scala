5aae5fc5db5f73f2d17316e6232799958db0bace
Stephane Landelle <slandelle@gatling.io>
2020-04-29 21:53:24 +0200

    Add test proving regex overlapping behavior

---
+++ input/src/main/scala/fix/regexoverlapping.scala
@@ -0,0 +1,14 @@
+/*
+ rule = SortImports
+ SortImports.blocks = [
+ "re:java.*",
+ "re:javax.*"
+ ]
+ */
+import javax.net.ssl.SSLEngine
+import java.math.BigInteger
+import java.util.Map
+
+object RegexOverlapping {
+  // Add code that needs fixing here.
+}
